f1d7119301479b56eec8448646826f27cf5699c2
nikitaspirin2004 <nikita.spirin2004@gmail.com>
2022-10-30 13:49:37 +0400

    praktika

--- couriers.py
+++ couriers.py
@@ -4,5 +4,13 @@ class point:
         self.y = y
     def __str__(self):
         return f'Tочки с координатами {self.x}, {self.y}'
-p1 = point(1,2)
-print(p1)
+    def get_distance(self, other_point):
+        x1 = self.x
+        x2 = other_point.x
+        y1 = self.y
+        y2 = other_point.y
+        dist = ((x2-x1)**2 + (y2-y1)**2)**0.5
+        return dist
+a = point(1,2)
+b = point(3,5)
+print (a.get_distance(b))
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ('Hello World!')
+class Banana:
+    def __init__(self,weight):
+        self.color = 'yellow'
+        self.weight = weight
+    def __cmp__(self, other):
+        return self.weight == other.weight
+b1 = Banana(12)
+b2 = Banana(12)
+print (b1 == b2)
