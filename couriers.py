cdc61205c570b6c1d6387a8a1ee5bf933d2f026a
nikitaspirin2004 <nikita.spirin2004@gmail.com>
2022-11-01 01:10:45 +0400

    gotovo

--- couriers.py
+++ couriers.py
@@ -1,4 +1,4 @@
-class point:
+class Point:
     def __init__(self,x,y):
         self.x = x
         self.y = y
@@ -11,6 +11,34 @@ class point:
         y2 = other_point.y
         dist = ((x2-x1)**2 + (y2-y1)**2)**0.5
         return dist
-a = point(1,2)
-b = point(3,5)
-print (a.get_distance(b))
+class Couriers:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+'''заказы'''
+z1 = Point(1,2)
+z2 = Point(3,4)
+z3 = Point(3,1)
+z4 = Point(3,7)
+z5 = Point (4,1)
+'''курьеры'''
+k1 = Couriers(3,2)
+k2 = Couriers(1,1)
+k3 = Couriers(2,4)
+k4 = Couriers(3,6)
+k5 = Couriers(1,5)
+zakazi = [z1,z2,z3,z4,z5]
+kurieri = [k1,k2,k3,k4,k5]
+for i in range (5):
+    kz = []
+    for k in range (5):
+        kz.append(zakazi[i].get_distance(kurieri[k]))
+    coirier = kz.index(min(kz))
+    min_distance = min(kz)
+    for k in range(5):
+        if min_distance == zakazi[i].get_distance(kurieri[k]):
+            kurieri[k].x = zakazi[i].x
+            kurieri[k].y = zakazi[i].y
+
+    print('Заказ №', i + 1, 'заберет', coirier + 1, "курьер.")
--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-class Banana:
-    def __init__(self,weight):
-        self.color = 'yellow'
-        self.weight = weight
-    def __cmp__(self, other):
-        return self.weight == other.weight
-b1 = Banana(12)
-b2 = Banana(12)
-print (b1 == b2)
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f'Tочки с координатами {self.x}, {self.y}'
+    def get_distance(self, other_point):
+        x1 = self.x
+        x2 = other_point.x
+        y1 = self.y
+        y2 = other_point.y
+        dist = ((x2-x1)**2 + (y2-y1)**2)**0.5
+        return dist
+class Couriers:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+'''заказы'''
+z1 = Point(2,2)
+z2 = Point(2,4)
+z3 = Point(1,1)
+
+'''курьеры'''
+k1 = Couriers(3,1)
+k2 = Couriers(4,6)
+k3 = Couriers(2,8)
+
+zakazi = [z1,z2,z3]
+kurieri = [k1,k2,k3]
+for i in range (3):
+    kz = []
+    for k in range (3):
+        kz.append(zakazi[i].get_distance(kurieri[k]))
+    coirier = kz.index(min(kz))
+    min_distance = min(kz)
+    for k in range(3):
+        if min_distance == zakazi[i].get_distance(kurieri[k]):
+            kurieri[k].x = zakazi[i].x
+            kurieri[k].y = zakazi[i].y
+
+    print('Заказ №', i + 1, 'заберет', coirier + 1, "курьер.")
